2f80764f1f407169b2b274098613ee2562aec7c2
mildredn <mildredn2108@gmail.com>
2017-11-07 13:28:32 -0500

    Test updates

--- public/index.php
+++ public/index.php
@@ -4,12 +4,10 @@
 	<p>Brief paragraph about IULMIA, maybe an introduction or a concise set of words describing the film collection at IULMIA. Now putting in filler lines to make this a sizeable paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla adipiscing, tortor rutrum adipiscing molestie, mauris purus ullamcorper felis, non tincidunt est lorem a est. Nullam blandit tincidunt pellentesque. Vivamus nisi nunc, tincidunt vel nisl vitae, luctus imperdiet risus. Vestibulum suscipit porta interdum. Proin vitae arcu eu enim feugiat adipiscing id sed justo. Mauris in risus elementum, aliquam orci non, venenatis ligula.</p>
 	<h1>Part of Indiana University?</h1>
 	<p>Indiana University uses your network username and password to login to IULMIA. Continue to login to IULMIA through your network.</p>
-	<form method="post" action ="/login">
-		<button class="button" type="submit">Continue</button>
-	</form>
+	<button class="button" type="submit" formaction="/login.php">Continue</button>
 	<div id="login-other-choices">
-		<a href="#">Not a part of Indiana University?</a> <!--This link should redirect to form(IULMIA)-->
+		<a href="/form.php">Not a part of Indiana University?</a> <!--This link should redirect to form(IULMIA)-->
 	</div>
 </div>
-
+<script>console.log('At index.php');</script>
 <?php include("../includes/templates/footer.php"); ?>
---
+++ public/login.php
@@ -0,0 +1,9 @@
+<?php include("cas.php"); ?>
+<html>
+  <head>
+    <title>CAS Example</title>
+  </head>
+  <body>
+    <h1>You're authenticated using CAS!</h1>
+  </body>
+</html> 
